a92d238050dd2e990429dfb7fde559d9bad24876
starengineer <mrg8406@gmail.com>
2024-05-01 23:17:00 -0700

    adjust the bootstrap css redirect for the user login create the migrations to create the tables of participants, brackets

---
+++ app/Database/Migrations/2024-05-02-053318_CreateParticipantsTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateParticipantsTable extends Migration
+{
+    public function up()
+    {
+        $attributes = ($this->db->getPlatform() === 'MySQLi') ? ['ENGINE' => 'InnoDB'] : [];
+
+        $this->forge->addField([
+            'id'             => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'name'       => ['type' => 'varchar', 'constraint' => 30, 'null' => 0],
+            'user_by'         => ['type' => 'int', 'constraint' => 11, 'null' => 0],
+            'order'         => ['type' => 'int', 'constraint' => 3, 'null' => true],
+            'active'         => ['type' => 'tinyint', 'constraint' => 1, 'null' => 0, 'default' => 0],
+            'created_at'     => ['type' => 'datetime', 'null' => true],
+            'updated_at'     => ['type' => 'datetime', 'null' => true],
+            'deleted_at'     => ['type' => 'datetime', 'null' => true],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('participants', false, $attributes);
+    }
+
+    public function down()
+    {
+        $this->db->disableForeignKeyChecks();
+
+        $this->forge->dropTable('participants', true);
+    }
+}
---
+++ app/Database/Migrations/2024-05-02-053421_CreateBracketsTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateBracketsTable extends Migration
+{
+    public function up()
+    {
+        $attributes = ($this->db->getPlatform() === 'MySQLi') ? ['ENGINE' => 'InnoDB'] : [];
+
+        $this->forge->addField([
+            'id'             => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'bracketNo'       => ['type' => 'int', 'constraint' => 3, 'null' => 0],
+            'bye'         => ['type' => 'tinyint', 'constraint' => 1, 'null' => 0],
+            'lastGames'         => ['type' => 'varchar', 'constraint' => 16, 'null' => true],
+            'nextGame'         => ['type' => 'int', 'constraint' => 1, 'null' => true, 'default' => 0],
+            'roundNo'         => ['type' => 'int', 'constraint' => 3, 'null' => 0],
+            'teamnames'         => ['type' => 'varchar', 'constraint' => 16, 'null' => true, 'default' => 0],
+            'winner'         => ['type' => 'int', 'constraint' => 11, 'null' => true],
+            'user_by'         => ['type' => 'int', 'constraint' => 11, 'null' => 0],
+            'final_result'         => ['type' => 'tinyint', 'constraint' => 1, 'null' => true, 'default' => 0],
+            'created_at'     => ['type' => 'datetime', 'null' => true],
+            'updated_at'     => ['type' => 'datetime', 'null' => true],
+            'deleted_at'     => ['type' => 'datetime', 'null' => true],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('brackets', false, $attributes);
+    }
+
+    public function down()
+    {
+        $this->db->disableForeignKeyChecks();
+
+        $this->forge->dropTable('brackets', true);
+    }
+}
