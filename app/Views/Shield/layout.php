e7b655a825dca0a9c9bb756f6a2d62698c0eef6a
StarEngineer89 <mrg8406@gmail.com>
2024-08-20 09:46:51 -0400

    add dashboard

--- app/Views/Shield/layout.php
+++ app/Views/Shield/layout.php
@@ -32,6 +32,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="<?= base_url('gallery') ?>">Tournament Gallery</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url('tournaments') ?>">My Tournament Dashboard</a>
+                        </li>
                     </ul>
                 </div>
 
