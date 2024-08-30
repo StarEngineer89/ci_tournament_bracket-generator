be12024bad6a6712f22d64217f21a4d6cec9912a
starengineer <mrg8406@gmail.com>
2024-08-30 00:18:36 -0700

    update db migration

--- app/Database/Migrations/2024-08-28-213259_DropForeignkeys.php
+++ app/Database/Migrations/2024-08-28-213259_DropForeignkeys.php
@@ -7,11 +7,7 @@ use CodeIgniter\Database\Migration;
 class DropForeignkeys extends Migration
 {
     public function up()
-    {
-        //
-        
-        $this->forge->dropForeignKey('tournaments', 'tournaments_user_id_foreign');
-        
+    {   
         $this->forge->dropForeignKey('share_settings', 'share_settings_user_id_foreign');
     }
 
