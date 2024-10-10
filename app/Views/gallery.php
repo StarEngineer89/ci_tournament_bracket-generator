a7776552abcb55527bc2a0407260bc3869c752f2
starengineer <mrg8406@gmail.com>
2024-10-10 14:01:22 -0700

    Fix issue

--- app/Views/gallery.php
+++ app/Views/gallery.php
@@ -110,7 +110,7 @@ tournamentsTable = $('#tournamentGalleryTable').DataTable({
                     return `
                         <div class="col-auto input-group">
                             <input type="text" class="form-control" id="tournamentURL_${row.id}" value="${row.public_url}" aria-label="Tournament URL" aria-describedby="urlCopy" readonly="">
-                            <button class="btn btn-outline-secondary input-group-text btnCopy" data-copyid="tournamentURL_${row.id}" type="button" data-toggle="popover" data-trigger="focus" data-placement="top" data-content="Link Copied!" onclick="copyClipboard(tournamentURL_${row.id})">Copy</button>
+                            <button class="btn btn-outline-secondary input-group-text btnCopy" data-copyid="tournamentURL_${row.id}" type="button" data-toggle="popover" data-trigger="focus" data-placement="top" data-content="Link Copied!" onclick="copyClipboard('tournamentURL_${row.id}')">Copy</button>
                         </div>
                         `
                 } else {
