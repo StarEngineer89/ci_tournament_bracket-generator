998a71dbe39df6cdebbff9acc5c99b0955e4fd17
starengineer <mrg8406@gmail.com>
2024-10-10 14:12:09 -0700

    Fix issue of foreign key check

--- app/Controllers/Api/BracketsController.php
+++ app/Controllers/Api/BracketsController.php
@@ -530,7 +530,7 @@ class BracketsController extends BaseController
 
             $db = \Config\Database::connect();
             $dbDriver = $db->DBDriver;
-            if (!auth()->user()) {
+            if (!auth()->user() && $dbDriver === 'MySQLi') {
                 $db->query('SET FOREIGN_KEY_CHECKS = 0;');
             }
 
@@ -542,7 +542,7 @@ class BracketsController extends BaseController
                                       ->setJSON(['status' => 'error', 'message' => 'Failed to save vote']);
             }
 
-            if (!auth()->user()) {
+            if (!auth()->user() && $dbDriver === 'MySQLi') {
                 $db->query('SET FOREIGN_KEY_CHECKS = 1;');
             }
         }
