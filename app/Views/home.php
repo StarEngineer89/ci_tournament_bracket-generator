61f4a55076522d464ab4efbd9bde5d7f89c370db
starengineer <mrg8406@gmail.com>
2025-01-16 17:07:28 -0800

    Update the styles

--- app/Views/home.php
+++ app/Views/home.php
@@ -16,26 +16,26 @@
     <div class="d-flex justify-content-center mb-3">
         <p class="home-content">
             Get started by creating your first tournament!<br />
-            <a class="btn btn-success" href="<?= base_url('/tournaments/create') ?>">Create Tournament</a>
+            <a class="btn btn-success mt-3" href="<?= base_url('/tournaments/create') ?>">Create Tournament</a>
         </p>
     </div>
     <div class="d-flex justify-content-center mb-3">
         <p class="home-content">
             Here to spectate? Visit the Tournament Gallery!<br />
-            <a class="btn btn-purple" href="<?= base_url('/gallery?filter=glr') ?>">Teleport to Gallery</a>
+            <a class="btn btn-purple mt-3" href="<?= base_url('/gallery?filter=glr') ?>">Teleport to Gallery</a>
         </p>
     </div>
     <div class="d-flex justify-content-center mb-3">
         <p class="home-content">
             Feeling the hype? Checkout the top contestants on the Participant Leaderboard!<br />
-            <a class="btn btn-orange" href="<?= base_url('/participants') ?>">Check Leaderboard</a>
+            <a class="btn btn-orange mt-3" href="<?= base_url('/participants') ?>">Check Leaderboard</a>
         </p>
     </div>
     <div class="d-flex justify-content-center mb-3">
         <p class="home-content">
             Want to manage/customize your tournaments?<br />
             Signup/Signin now to access your own dedicated Tournament Dashboard!<br />
-            <a class="btn btn-warning" href="<?= base_url('/tournaments') ?>">My Tournament Dashboard</a>
+            <a class="btn btn-warning mt-3" href="<?= base_url('/tournaments') ?>">My Tournament Dashboard</a>
         </p>
     </div>
 </div>
