0843a119d29da66633f8d6242c48024b0c8d720c
starengineer <mrg8406@gmail.com>
2025-02-10 20:02:18 -0800

    add the Note on the registartion

--- app/Views/Shield/register.php
+++ app/Views/Shield/register.php
@@ -10,7 +10,7 @@
                 <h5 class="card-title mb-5"><?= lang('Auth.register') ?></h5>
 
                 <?php if (session('error') !== null) : ?>
-                <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+                <div class="alert alert-danger" role="alert"><?= session('error') ?><?= "<br/>Use the login link function if you've forgotten your account credentials or try using different credentials if registering for a new account." ?></div>
                 <?php elseif (session('errors') !== null) : ?>
                 <div class="alert alert-danger" role="alert">
                     <?php if (is_array(session('errors'))) : ?>
@@ -20,6 +20,7 @@
                     <?php else : ?>
                     <?= session('errors') ?>
                     <?php endif ?>
+                    <?= "<br/>Use the login link function if you've forgotten your account credentials or try using different credentials if registering for a new account." ?>
                 </div>
                 <?php endif ?>
 
