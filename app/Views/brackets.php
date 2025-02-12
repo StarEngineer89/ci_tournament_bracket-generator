ef82d12e28dbb7c2a098352ea6bebfc723d06790
starengineer <mrg8406@gmail.com>
2025-02-12 04:18:22 -0800

    fix issue on the vote availability alert

--- app/Views/brackets.php
+++ app/Views/brackets.php
@@ -325,7 +325,7 @@ $(document).ready(function() {
 
         <div id="availabilityAlertPlaceholder"></div>
         <div id="availabilityAlertMsg" class="d-none">
-            The tournament <strong><?= $tournament['name'] ?></strong> will be available starting <?= convert_to_user_timezone($tournament['available_start'], user_timezone(auth()->user()->id)) ?> and ending on <?= convert_to_user_timezone($tournament['available_end'], user_timezone(auth()->user()->id)) ?>. <br />
+            The tournament <strong><?= $tournament['name'] ?></strong> will be available starting <?= auth()->user() ? convert_to_user_timezone($tournament['available_start'], user_timezone(auth()->user()->id)) : $tournament['available_start'] ?> and ending on <?= auth()->user() ? convert_to_user_timezone($tournament['available_end'], user_timezone(auth()->user()->id)) : $tournament['available_end'] ?>. <br />
             If voting is enabled, the voting period will begin once the tournament availability starts and conclude once the availability ends.
         </div>
 
