6119262bd0daf34e5997ba54d46912c21e71cbd7
starengineer <mrg8406@gmail.com>
2025-03-14 11:34:36 -0700

    Fix issue

--- app/Views/brackets.php
+++ app/Views/brackets.php
@@ -421,7 +421,11 @@ $(document).ready(function() {
         </nav>
         <h5 class="card-title d-flex justify-content-center mb-5">
             <?= $tournament['name'] ?>&nbsp;
-            <button type="button" class="btn btn-light p-0 bg-transparent border-0" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-html="true" data-bs-content="<strong>Organized/Hosted by</strong>:<br/><?= $tournament['created_by'] ?> (<?= $tournament['created_by']->email ?>)"><i class="fa-classic fa-solid fa-circle-exclamation"></i></button>
+            <?php if ($tournament['created_by']): ?>
+            <button type="button" class="btn btn-light p-0 bg-transparent border-0" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-html="true" data-bs-content="<strong>Organized/Hosted by</strong>:<br/><?= $tournament['created_by']->username ?> (<?= $tournament['created_by']->email ?>)"><i class="fa-classic fa-solid fa-circle-exclamation"></i></button>
+            <?php else: ?>
+            <button type="button" class="btn btn-light p-0 bg-transparent border-0" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-html="true" data-bs-content="<strong>Organized/Hosted by</strong>:<br/>Guest"><i class="fa-classic fa-solid fa-circle-exclamation"></i></button>
+            <?php endif ?>
         </h5>
 
         <?php if (session('error') !== null) : ?>
